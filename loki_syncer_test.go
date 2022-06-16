9ac0a6185a8bd9a70f8b92ad3a7b59c0db82c953
miiton <468745+miiton@users.noreply.github.com>
2022-06-16 11:33:59 +0900

    Add a docker-compose.yml for Loki

--- loki_syncer_test.go
+++ loki_syncer_test.go
@@ -1,1 +1,23 @@
 package miilog
+
+import "testing"
+
+func Test_SetLoggerProductionWithLokiMust(t *testing.T) {
+	SetLoggerProductionWithLokiMust("http://localhost:3100", "MYAWESOMETENANT", "{host=\"localhost\", job=\"gotest\"}")
+	defer Sync()
+	Info("foo")
+	Info("bar")
+	Info("baz")
+	Info("hoge")
+	Info("fuga")
+	Info("piyo")
+	Info("foobar")
+	Info("bazbaz")
+	Info("hogehoge")
+	Info("fugafuga")
+	Info("piyopiyo")
+	Info("hogefuga")
+	Info("hogepiyo")
+	Info("fugapiyo")
+	Info("hogefugapiyo")
+}
